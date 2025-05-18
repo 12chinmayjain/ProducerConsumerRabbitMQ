9613c64ffc0f3dd4f5c41df8721eb6ebd1136162
cjain <chinmayjain2702@gmail.com>
2025-05-18 18:03:02 +0530

    Add project files.

---
+++ Sender.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Sender
+    {
+        static void Main(string[] args)
+        {
+           var factory = new ConnectionFactory() { HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                string message = "Getting started with .net core rabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("","BasicTest",null,body);
+                Console.WriteLine("Sent Message",message);
+            }
+            Console.WriteLine("Press [enter] to exit the sender app...");
+            Console.WriteLine();
+        }
+    }
+}
